9f432d46f05319501cae9806913bcc33da413948
morozovdanil111-prog <morozovdanil111@gmail.com>
2026-03-06 23:40:29 +0300

    Update handlers.go

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -7,7 +7,7 @@ import (
 	"os"
 	"path/filepath"
 	"time"
-	"6/service" // Путь к вашему пакету service
+	"6sprint/6/service" // Путь к вашему пакету service
 )
 
 // RootHandler возвращает HTML-форму на корневой путь "/"
